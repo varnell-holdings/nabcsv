1c58d515e73770b5ca719a143e6827c13520a9c4
John Tillett <tillett1957@gmail.com>
2018-01-14 12:34:15 +1100

    added tests

--- nab_income_adder.py
+++ nab_income_adder.py
@@ -10,10 +10,16 @@ def clean_up(inc):
     return inc
 
 
-download = input('Enter the csv file name you have just downloaded. ')
-download = '/Users/jtair/Downloads/' + download
-with open(download, 'r') as f:
+def main(download):
+    with open(download, 'r') as f:
         rows = csv.reader(f)
         next(rows)
         total = sum(clean_up(row[1]) for row in rows)
-        print('Total income is {: .2f}'.format(total))
+        return round(total, 2)
+
+
+if __name__ == '__main__':
+    download = input('Enter the csv file name you have just downloaded. ')
+    download = '/Users/jtair/Downloads/' + download
+    total = main(download)
+    print('Total income is {: .2f}'.format(total))
---
+++ tests/test_nab.py
@@ -0,0 +1,21 @@
+import csv
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import nab_income_adder as nab
+
+
+def test_1():
+    test_data_1 = [['name', 'inc', 'blub'],
+                   ['john', '+$1,000.00', 'uiop'],
+                   ['pete', '+$1,000.05', 'uiop']]
+    with open('data_1.csv', 'w') as f:
+        writer = csv.writer(f)
+        for row in test_data_1:
+            writer.writerow(row)
+    assert nab.main('data_1.csv') == 2000.05
+
+
+def test_2():
+    assert nab.main('TransactionHistory-25.csv') == 213234.33
