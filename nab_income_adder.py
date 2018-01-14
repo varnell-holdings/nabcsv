3ca82404879edb4a296e3bb9afd5d84d7242121d
John Tillett <tillett1957@gmail.com>
2018-01-14 11:10:14 +1100

    initial commit to with_generator

--- nab_income_adder.py
+++ nab_income_adder.py
@@ -3,15 +3,17 @@
 import csv
 
 
+def clean_up(inc):
+    inc = inc.strip('$+')
+    inc = inc.replace(',', '')
+    inc = float(inc)
+    return inc
+
+
 download = input('Enter the csv file name you have just downloaded. ')
 download = '/Users/jtair/Downloads/' + download
 with open(download, 'r') as f:
         rows = csv.reader(f)
         next(rows)
-        total = 0
-        for row in rows:
-            inc = row[1]
-            inc = inc.strip('$+')
-            inc = inc.replace(',', '')
-            total += float(inc)
+        total = sum(clean_up(row[1]) for row in rows)
         print('Total income is {: .2f}'.format(total))
